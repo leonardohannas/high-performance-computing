e9c175d3d8748433eddb8310cd8f0e8c2fe40900
LeonardoHannas <68200228+LeonardoHannas@users.noreply.github.com>
2026-04-12 17:23:07 -0300

    read_input_file function added

--- src/studentsseq.c
+++ src/studentsseq.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
+#define MEMORY_ALLOCATION_ERROR 1
+
+
+void read_input_file(const char* filename, int* R, int* C, int* A, int* N, int* T, int* seed) {
     FILE *f;
-    f = fopen("../docs/exemplo_entrada_0.txt", "r");
+    f = fopen(filename, "r");
     if (f == NULL) {
         printf("Erro ao abrir o arquivo!\n");
-        return 1;
+        exit(MEMORY_ALLOCATION_ERROR);
     }
-    int R, C, A, N, T, seed;
-    fscanf(f, "%d %d %d %d %d %d", &R, &C, &A, &N, &T, &seed);
-    printf("R: %d, C: %d, A: %d, N: %d, T: %d, seed: %d\n", R, C, A, N, T, seed);
+
+    fscanf(f, "%d %d %d %d %d %d", R, C, A, N, T, seed);
+    printf("R: %d, C: %d, A: %d, N: %d, T: %d, seed: %d\n", *R, *C, *A, *N, *T, *seed);
     fclose(f);
-    return 0;
 }
+
+
+int main(int argc, char* argv[]) {
+
+    int R, C, A, N, T, seed;
+    read_input_file("../docs/exemplo_entrada_0.txt", &R, &C, &A, &N, &T, &seed);
+ 
+
+    // int** matrix_student_grade = (int**) malloc(sizeof(int*) * R * C * A);
+    // if (matrix_student_grade == NULL) {
+    //     printf("Erro ao alocar memória para a matriz de notas dos alunos!\n");
+    //     return MEMORY_ALLOCATION_ERROR;
+    // }
+    // for (int i = 0; i < R * C * A; i++) {
+    //     matrix_student_grade[i] = (int*) malloc(sizeof(int) * N);
+    //     if (matrix_student_grade[i] == NULL) {
+    //         printf("Erro ao alocar memória para a linha %d da matriz de notas dos alunos!\n", i);
+    //         return MEMORY_ALLOCATION_ERROR;
+    //     }
+    // }
+
+
+
+    return 0;
+
+}
+
